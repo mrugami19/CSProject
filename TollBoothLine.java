edd2713f9b60b47f6790bbebdb4e40b03de9497f
phodonou <folucho@gmail.com>
2017-04-22 06:41:11 -0500

    Add files via upload

--- TollBoothLine.java
+++ TollBoothLine.java
@@ -5,7 +5,8 @@ public class TollBoothLine
 	private static int numLineAvailable = 6;
 	private Vehicle line[][];
 	private int numItems[];//how many items are in each line
-	private int currentpos[];
+	private int currentPos[];
+	private boolean isFull[];
 	
 	
 	
@@ -13,28 +14,74 @@ public class TollBoothLine
 	{
 		if((numLine > 0) && ( (numLineAvailable - numLine)>=0 ))
 		{
-			line = new Vehicle[numLine][];
+			line = new Vehicle[numLine][MAX_SIZE];
 			numLineAvailable -= numLine;//decrease numLineAvailable
+			
+			numItems = new int [numLine];
 			for(int i =0; i < numLine; i++)//Initialize numItems to 0
 			{
 				numItems[i]=0;
+			}
+			
+			currentPos = new int [numLine];
+			for(int i =0; i < numLine; i++)//Initialize currentPos to 0
+			{
+				currentPos[i]=0;
+			}
+			
+			isFull = new boolean[numLine];
+			for(int i=0; i < numLine; i++)//Initialize isFull to false at first
+			{
+				isFull[i] = false;
 			}
 		}
 		else 
 			System.out.println("EXCEPTION");//ADD AN EXCEPTION LATER
 				
 	}
-	 
+	
 	
 	public boolean insert(Vehicle insVeh)
 	{
+		boolean vehicleInserted = false;
+		int lowestIndex =0;//Index of the line with the least number of cars
+		int lowestLine = numItems[0]; //Assume that the first line has the least number of cars
 		
+		for(int i =0; i<numItems.length; i++)//Checks to see which line has the lowest amount of cars
+		{
+			if((numItems[i] < lowestLine))
+			{
+				lowestLine = numItems[i];
+				lowestIndex = i;
+				
+			}
+		}
+		
+		if(numItems[lowestIndex] < MAX_SIZE)// check if line with the least amount of vehicles has less then 25 cars
+		{
+			line[lowestIndex][currentPos[lowestIndex]] = insVeh;//if so, insert it there
+			vehicleInserted = true;
+			Vehicle.setLine(lowestIndex);//saves the line this car was inserted to into an attribute
+			numItems[lowestIndex]++; //increase number of cars of that line
+			currentPos[lowestIndex]++; //increase current position of that line	
+			
+			if( (numItems[lowestIndex]) == MAX_SIZE)//If the line is 25, then its full
+			{
+				isFull[lowestIndex] = true;
+			}
+		}
+		System.out.println("Is full: " + isFull[lowestIndex]);
+		
+		return vehicleInserted;
 	}
 	
 	public boolean remove(Vehicle remVeh)
 	{
+		boolean vehicleRemoved = false;
 		
+		return vehicleRemoved;
 	}
+	
 	
 	
 	
--- test.java
+++ test.java
@@ -3,10 +3,22 @@ public class test
 {
 	public static void main(String[] args)
 	{
-		int numOfManual = 4;
-		int numOfAuto = 2;
+		int numOfManual = 6;
+//		int numOfAuto = 2;
 		TollBoothLine manual = new TollBoothLine(numOfManual);
-		TollBoothLine automatic = new TollBoothLine(numOfAuto);
+//		TollBoothLine automatic = new TollBoothLine(numOfAuto);
+		
+		
+		//Tests if vehicle is inserted into the right pos
+//		Vehicle v1 = new Vehicle();
+//		System.out.println(manual.insert(v1));
+//		
+		
+		//Test that it doesn't add a car in a full line
+		for(int i =0; i < 150; i++)
+		{
+			manual.insert(new Vehicle());
+		}
 		
 	}
 }
