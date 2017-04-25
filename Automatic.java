8dc66889337351fa501ef75ab272b2f8f50c5e31
Ashma Manandhar <ashmamdr11@gmail.com>
2017-04-24 19:35:47 -0500

    Update Automatic.java

--- Automatic.java
+++ Automatic.java
@@ -3,4 +3,7 @@ public class Automatic extends Vehicle{
 	public Automatic(boolean type, int numWheels, int arrTime){
 		super(type,numWheels,arrTime);
 	}
+	public String toString(){
+		return super.toString() + super.getWheels();
+	}
 }
